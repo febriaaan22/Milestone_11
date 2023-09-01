4f91258a669934992343b36da2bee56fb1496fe5
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2023-09-01 19:34:17 +0700

    Open API changing

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const authMiddleware = require('./middleware/authentication-middleware.js')
 
 const swaggerUi = require('swagger-ui-express')
 const yaml = require('yaml')
-const openApiValidator = require('express-openapi-validator')
+// const openApiValidator = require('express-openapi-validator')
 const bodyParser = require('body-parser')
 
 const app = express()
@@ -17,9 +17,9 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(yaml.parse(require('fs').readFileSync('./swagger/API.yaml', 'utf8'))))
-app.use(openApiValidator.middleware({
-    apiSpec: './swagger/API.yaml'
-}));
+// app.use(openApiValidator.middleware({
+//     apiSpec: './swagger/API.yaml'
+// }));
 app.use(databaseMiddleware)
 
 app.get('/', (req, res) => {
